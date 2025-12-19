8670a33817fe2ac3b4b79febbd53f3114f9be613
kritimatharu <kritimatharu01>
2025-12-19 22:14:33 +0545

    Thrid day

---
+++ string_Methods.dart
@@ -0,0 +1,149 @@
+//Declare a string "Dart" and print its length.
+
+// void main() {
+//   String Language= "Dart";
+//    print(Language.length);
+// }
+
+//Create an empty string and check whether it is empty using a string property.
+// void main() {
+//   var name = "";
+//   print(name.isEmpty);
+// }
+
+//Write a program that prints "Empty" if a string has no characters; otherwise print "Not Empty".
+// void main() {
+//   String text = "Ram";
+//   if (text.isEmpty) {
+//     print("Empty");
+//   } else {
+//     print("Not Empty");
+//   }
+// }
+
+//Check whether the string "Flutter" is not empty and print the result.
+// void main() {
+//   String text = "Flutter";
+//   bool isNotEmpty = text.isNotEmpty;
+//   print(isNotEmpty);
+// }
+
+//Given a string "Hello World", print:
+// The length of the string
+// Whether the string is empty or not
+
+// void main() {
+//   var text = "Hello World";
+//   if (text.length == 0) {
+//     print("empty");
+//   } else {
+//     print(text.length);
+//   }
+// }
+
+//Write a function that accepts a string and prints "Valid" if its length is greater than 5; otherwise print "Invalid"
+// void valid(text) {
+//   if (text.length > 5) {
+//     print("valid");
+//   } else {
+//     print("invlaid");
+//   }
+// }
+
+// void main() {
+//   valid("this is text");
+// }
+
+//Convert the string "dart" to uppercase and print it.
+
+// void main() {
+//   var program = "dart";
+//   print(program.toUpperCase());
+// }
+
+//Convert the string "FLUTTER" to lowercase and print it.
+// import 'dart:async';
+
+// void main() {
+//   String program = "FLUTTER";
+//   print(program.toLowerCase());
+// }
+
+//Write a program that checks whether a string remains the same after converting it to uppercase.
+// void main() {
+//   String text = "Flutter";
+//   String capitalText = text.toUpperCase();
+//   if (text == capitalText) {
+//     print("same");
+//   } else {
+//     print("different");
+//   }
+// }
+
+//Given a list of names, convert all names to lowercase and print the list.
+// void main() {
+//   var names = ["ASMITA", "SALONI", "RAM", "SITA"];
+//   var lowerName = names.map((name) => name.toLowerCase());
+//   print(lowerName);
+// }
+
+//Write a function that converts a string to uppercase only if it is not empty.
+
+// void touppercase(text) {
+//   if (text.length==0) {
+//     print("empty");
+//   } else {
+//     print(text.toUpperCase());
+//   }
+// }
+
+// void main() {
+//   touppercase("kritima");
+// }
+
+//Convert the string "Hello Dart" entirely to lowercase and print its length.
+// void main() {
+//   String text = "Hello Dart";
+//   String lowertext = text.toLowerCase();
+//   print(lowertext.length);
+// }
+
+//Check if the string "Dart Programming" contains the word "Dart"
+// void main() {
+//   var Course = "Dart Programming";
+//   if (Course.contains("Dart")) {
+//     print("it contains dart");
+//   } else {
+//     print("it donot conatins dart");
+//   }
+// }
+
+//Determine if the string "Flutter" starts with "Flu"
+// void main() {
+//   var text = "Flutter";
+//   print(text.startsWith("Flu"));
+// }
+
+//Determine if the string "example.txt" ends with ".txt"
+// void main() {
+//   String text = "example.txt";
+//   print(text.endsWith(".txt"));
+// }
+
+//Find the first occurrence of the letter "a" in the string "banana"
+
+// void main() {
+//   var text = "banana";
+//   var index = text.indexOf("a");
+//   print(index);
+// }
+
+//Write a program to check whether a string starts with "Hi" or ends with "!"
+// void main() {
+//   var text = "Hi  !";
+//   if (text.startsWith("Hi") || text.endsWith("!")) {
+//     print("start Hi");
+//   } else {
+//     print("ends !");
+//   }
+// }
