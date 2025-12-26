80e4b06c414a417de72473bdf80587a86c4cb065
kritimatharu <kritimatharu01>
2025-12-26 22:04:47 +0545

    Eighth day

---
+++ Condition.dart
@@ -0,0 +1,102 @@
+import 'dart:async';
+
+////Write a program that checks whether a number is positive or negative
+
+// void main() {
+//   int number = -5;
+//   if (number > 0) {
+//     print("Number is positive");
+//   } else if (number < 0) {
+//     print("Number is Negative");
+//   } else {
+//     print("Number is Zero");
+//   }
+// }
+
+//Write a program to check if a number is even or odd.
+// void main() {
+//   int number = 7;
+//   if (number % 2 == 0) {
+//     print("Even number");
+//   } else {
+//     print("Odd number");
+//   }
+// }
+
+//Write a program that prints "Pass" if marks are 40 or above, otherwise prints "Fail"
+
+// void main() {
+//   int marks = 50;
+//   if (marks >= 40) {
+//     print("Pass");
+//   } else {
+//     print("Fail");
+//   }
+// }
+
+//Write a program that checks whether a given number is greater than 100.
+// void main() {
+//   int number = 120;
+//   if (number > 100) {
+//     print("The number is greater than 100");
+//   } else {
+//     print("The number is not greater than 100");
+//   }
+// }
+
+//Write a program that checks whether a string is empty or not.
+// void main() {
+//   String text = "";
+//   if (text.isEmpty) {
+//     print("The string is empty");
+//   } else {
+//     print("The string is not empty");
+//   }
+// }
+
+//Write a program to find the largest of two numbers using if–else.
+// void main() {
+//   int a = 20;
+//   int b = 30;
+//   if (a > b) {
+//     print("A is the largest number");
+//   } else if (b > a) {
+//     print("B is the largest number");
+//   } else {
+//     print("Both number is equals");
+//   }
+// }
+
+//Write a program to check whether a year is a leap year.
+
+// void main() {
+//   int year = 2024;
+//   if (year % 4 == 0) {
+//     if (year % 100 == 0) {
+//       if (year % 400 == 0) {
+//         print("Leap year");
+//       } else {
+//         print("Not a leap year");
+//       }
+//     } else {
+//       print("Leap year");
+//     }
+//   } else {
+//     print("Not a leap year");
+//   }
+// }
+
+//Write a program to display the grade based on marks: 80 and above → A 60–79 → B 40–59 → C Below 40 → Fail
+
+// void main() {
+//   int marks = 72;
+//   if (marks >= 80) {
+//     print("Grade:A");
+//   } else if (marks >= 60) {
+//     print("Grade:B");
+//   } else if (marks >= 40) {
+//     print("Grade: C");
+//   } else {
+//     print("Fail");
+//   }
+// }
---
+++ Forloop.dart
@@ -0,0 +1,39 @@
+//Print numbers from 1 to 10 using a for loop.
+// void main() {
+//   for (int i = 1; i <= 10; i++) {
+//     print("i");
+//   }
+// }
+
+//Print numbers from 10 to 1 using a while loop
+// void main() {
+//   int i = 10;
+//   while (i >= 1) {
+//     print("i");
+//     i--;
+//   }
+// }
+
+//Print the sum of first 10 natural numbers
+// void main() {
+//   int sum = 0;
+//   for (int i = 1; i <= 10; i++) {
+//     sum += i;
+//   }
+//   print("Sum of first 10 natural numbers is: $sum");
+// }
+
+//Count the number of digits in a given number
+// void main() {
+//   int number = 1234;
+//   int count = 0;
+//   if (number == 0) {
+//     count + 1;
+//   } else {
+//     while (number != 0) {
+//       number ~/= 10;
+//       count++;
+//     }
+//   }
+//   print("Number of digits:$count");
+// }
