d835878167a19fcc61ed1379e74381c22a408361
kritimatharu <kritimatharu01>
2025-12-27 20:09:41 +0545

    Ninth day

---
+++ Whileloop.dart
@@ -0,0 +1,75 @@
+//Print numbers from 1 to 20 using a while loop.
+// void main() {
+//   int i = 1;
+//   while (i <= 20) {
+//     print(i);
+//     i++;
+//   }
+// }
+
+//Print numbers from 20 to 1 using a while loop
+// void main() {
+// int i = 20;
+//   while (i >= 1) {
+//     print(i);
+//     i--;
+//   }
+
+// }
+
+//Print all even numbers between 1 and 50 using a while loop.
+// void main() {
+//   int i = 2;
+//   while (i <= 50) {
+//     print(i);
+//     i += 2;
+//   }
+// }
+
+//Check whether a number is a palindrome using a while loop
+
+// void main() {
+//   int number = 123;
+//   int originalNumnber = number;
+//   int reverse = 0;
+
+//   while (reverse != 0) {
+//     int digit = number % 10;
+//     reverse = reverse * 10 + digit;
+//     number ~/= 10;
+//   }
+//   if (originalNumnber == reverse) {
+//     print("Palindrome numner");
+//   } else {
+//     print("Not Palindrome number");
+//   }
+// }
+
+//Check whether a number is an Armstrong number using a while loop.
+
+// void main() {
+//   int number = 153;
+//   int originalNumber = number;
+//   int sum = 0;
+//   while (number != 0) {
+//     int digit = number % 10;
+//     sum += digit * digit * digit;
+//     number ~/= 10;
+//   }
+//   if (sum == originalNumber) {
+//     print("Armstrong number");
+//   } else {
+//     print("Not Armstrong number");
+//   }
+// }
+
+//Print numbers from 1 to 100, skipping multiples of 5.
+// void main() {
+//   int i = 1;
+//   while (i <= 100) {
+//     if (i % 5 != 0) {
+//       print(i);
+//     }
+//     i++;
+//   }
+// }
